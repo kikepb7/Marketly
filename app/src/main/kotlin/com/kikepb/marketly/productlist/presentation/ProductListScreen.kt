781f04942a9cd0d2d967c7e911f1903466f65c45
kikepb7 <enriquepalmadev@gmail.com>
2026-03-14 13:28:13 +0100

    DI | Include hilt dependency injection structure

---
+++ app/src/main/kotlin/com/kikepb/marketly/productlist/presentation/ProductListScreen.kt
@@ -0,0 +1,11 @@
+package com.kikepb.marketly.productlist.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+
+@Composable
+fun ProductListRoot(
+    productListViewModel: ProductListViewModel = hiltViewModel()
+) {
+
+}
---
+++ app/src/main/kotlin/com/kikepb/marketly/productlist/presentation/ProductListViewModel.kt
@@ -0,0 +1,10 @@
+package com.kikepb.marketly.productlist.presentation
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ProductListViewModel @Inject constructor(): ViewModel() {
+
+}
